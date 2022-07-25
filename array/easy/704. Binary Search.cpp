ab809a4fba9a2ce17d4d4b3934c5ffd705ce1248
Forhadul Islam <fitareq@gmail.com>
2022-07-25 13:28:01 +0600

    "array/easy/704. Binary Search.cpp": create

---
+++ array/easy/704. Binary Search.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n-1;
+        while (low <= high)
+        {
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target) return mid;
+            else if(target>nums[mid]) low = mid+1;
+            else  high = mid-1;
+        }
+
+        return -1;
+        
+    }
+};
