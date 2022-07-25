e6c6fb2aceb3bad97fe2e53a6f523c9f3b137dd6
Forhadul Islam <fitareq@gmail.com>
2022-07-25 13:27:35 +0600

    "array/easy/217. Contains Duplicate.cpp": create

---
+++ array/easy/217. Contains Duplicate.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      
+return nums.size() > set<int> (nums.begin(), nums.end()).size();
+    }
+};
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      
+return nums.size() > set<int> (nums.begin(), nums.end()).size();
+    }
+};
