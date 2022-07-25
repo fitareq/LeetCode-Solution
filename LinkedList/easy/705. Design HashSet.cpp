7d5e4933c27fe2cf922779a2816bd69450fd59ec
Forhadul Islam <fitareq@gmail.com>
2022-07-25 17:38:35 +0600

    "LinkedList/easy/705. Design HashSet.cpp": create

---
+++ LinkedList/easy/705. Design HashSet.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyHashSet {
+    vector<int> v;
+public:
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        auto it = find(v.begin(), v.end(), key);
+        if(it == v.end()){
+            v.push_back(key); 
+        }
+       
+    }
+    
+    void remove(int key) {
+        auto it = find(v.begin(), v.end(), key);
+        if(it != v.end()){
+            v.erase(it);
+        }
+    }
+    
+    bool contains(int key) {
+        return find(v.begin(), v.end(), key) != v.end();
+    }
+};
