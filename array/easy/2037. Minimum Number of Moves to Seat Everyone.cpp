db400302a407765297b1cf1c4983e346e1be1b4a
Forhadul Islam <fitareq@gmail.com>
2022-07-25 13:24:49 +0600

    "array/easy/2037. Minimum Number of Moves to Seat Everyone.cpp": create

---
+++ array/easy/2037. Minimum Number of Moves to Seat Everyone.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;  
+
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        int moves = 0;
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        
+        for (int i = 0; i < seats.size(); i++)
+        {
+            moves += abs(students[i]-seats[i]);
+        }
+       return moves; 
+    }
+};
