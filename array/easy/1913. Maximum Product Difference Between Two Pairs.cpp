f5915bed9e6c87c6cec6630cd5aca83ab2e27396
Forhadul Islam <fitareq@gmail.com>
2022-07-25 13:26:07 +0600

    "array/easy/1913. Maximum Product Difference Between Two Pairs.cpp": create

---
+++ array/easy/1913. Maximum Product Difference Between Two Pairs.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;  
+
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+       sort(nums.begin(), nums.end());
+       return nums[nums.size()-1]*nums[nums.size()-2] - nums[0]*nums[1];
+    }
+};
