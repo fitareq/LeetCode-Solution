b279e411b9598e66a3dfa91835be8e422b92cdaf
Forhadul Islam <fitareq@gmail.com>
2022-07-25 13:22:36 +0600

    "LinkedList/easy/1290. Convert Binary Number in a Linked List to Integer.cpp": create

---
+++ LinkedList/easy/1290. Convert Binary Number in a Linked List to Integer.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+
+public:
+    int getDecimalValue(ListNode* head) {
+        int res = head -> val;
+        while (head -> next)
+        {
+            res = (res << 1) | head -> next->val;
+            head = head -> next;
+        }
+        return res;
+    }
+};
