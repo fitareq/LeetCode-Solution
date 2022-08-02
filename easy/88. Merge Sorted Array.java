7e8189a735ef49ff94f7ae11f248254d29ca7383
Forhadul Islam <fitareq@gmail.com>
2022-08-02 19:11:17 +0600

    "easy/88. Merge Sorted Array.java":

--- easy/88. Merge Sorted Array.java
+++ easy/88. Merge Sorted Array.java
@@ -1,9 +1,8 @@
 class Solution {
-    public:
-        void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-            for(int i = 0; i<n; i++){
-                nums1[m+i] = nums2[i];
-            }
-            sort(nums1.begin(), nums2.end());
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i = 0; i<n; i++){
+            nums1[m+i] = nums2[i];
         }
-    };
+        Arrays.sort(nums1);
+    }
+}
