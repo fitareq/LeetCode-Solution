022d791916df67345baa9a5e3ca5a7d6429f5bbc
Forhadul Islam <fitareq@gmail.com>
2022-10-10 15:51:49 +0600

    Time: 0 ms (100.00%), Space: 6.3 MB (5.00%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int n = palindrome.length();
+        for(int i = 0; i<n/2; i++){
+            if(palindrome[i] != 'a'){
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+        }
+        
+        palindrome[n-1] = 'b';
+        return n<2?"":palindrome;
+    }
+};
