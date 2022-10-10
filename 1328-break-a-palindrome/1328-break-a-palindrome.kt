cb31e9b72d1c3da39a8da0172229cbeadb895517
Forhadul Islam <fitareq@gmail.com>
2022-10-10 15:37:48 +0600

    Time: 276 ms (25.00%), Space: 34.3 MB (50.00%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.kt
+++ 1328-break-a-palindrome/1328-break-a-palindrome.kt
@@ -1,21 +1,17 @@
 class Solution {
-    fun breakPalindrome(p: String): String {
-        if (p.length == 1)
-            return ""
-        val q = p.toCharArray()
-        for (i in p.indices)
-        {
-            if (q[i] != 'a'){
-                q[i] = 'a'
-                println(q)
-                val v = q.joinToString ( "" )
-                println(v)
-                if (q.reversed().joinToString("") != q.joinToString(""))
-                    return q.joinToString("")
+        fun breakPalindrome(p: String): String {
+            val n = p.length
+            if (n<2) return ""
+            val sb = StringBuilder(p)
+            for(i in 0 until n/2){
+                if(sb[i] != 'a')
+                {
+                    sb[i]= 'a'
+                    return sb.toString()
+                }
             }
+            sb[n-1] = 'b'
+
+            return sb.toString()
         }
-        val x = p.toCharArray()
-        x[p.length-1] = 'b'
-        return x.joinToString("")
     }
-}
