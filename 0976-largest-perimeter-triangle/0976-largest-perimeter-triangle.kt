87739943a16c98b781f1e13801886602992aed84
Forhadul Islam <fitareq@gmail.com>
2022-10-12 11:24:28 +0600

    Time: 519 ms (80.43%), Space: 57.8 MB (63.04%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.kt
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.kt
@@ -1,14 +1,10 @@
 class Solution {
         fun largestPerimeter(nums: IntArray): Int {
-            val num = nums.sortedArrayDescending()
-            var sum = 0
+           nums.sort()
+            for(i in nums.size-1 downTo 2)
+            if (nums[i]<nums[i-1]+nums[i-2])
+                return nums[i]+nums[i-1]+nums[i-2]
             
-            for(i in 0..num.size-3)
-            if (num[i]+num[i+1]<= num[i+2] || num[i]+num[i+2]<= num[i+1] || num[i+1]+num[i+2]<= num[i])
-                continue
-            else {
-                if(num[i]+num[i+1]+num[i+2]>sum) sum = num[i]+num[i+1]+num[i+2]
-            }
-            return sum
+            return 0
         }
     }
